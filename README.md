f381d951fce8d2e889486f56189642c1424c3d6e
Irina <iyurasowa@yandex.ru>
2025-04-15 20:07:35 +0700

    Проектная работа, файлы проекта

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/FlcHub/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,46 @@
-// @todo: Темплейт карточки
+// Создает элемент карточки для вывода в html
+// * card информация о карточке (словарь):
+//   name: String
+//   link: String
+// * deleteCb - колбэк для удаления карточки
+function addCard(card, deleteCb) {
+  // клонировать шаблон
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: DOM узлы
+  // установить значения вложенных элементов
+  const imageElement = cardElement.querySelector('.card__image');
+  imageElement.src = card.link;
+  imageElement.alt = card.name;
 
-// @todo: Функция создания карточки
+  const descriptionElement = cardElement.querySelector('.card__description .card__title');
+  descriptionElement.textContent = card.name;
 
-// @todo: Функция удаления карточки
+  // добавить к иконке удаления обработчик клика,
+  // по которому будет вызван deleteCb
+  const deleteButton  = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCb);
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+}
+
+// Удаляет карточку
+function deleteCard(evt) {
+  const deleteButton = evt.target;
+  const listItem = deleteButton.closest('.places__item');
+  listItem.remove();
+}
+
+// Выводит карточки на страницу
+function showAllCards() {
+  initialCards.forEach((item) => {
+    // создать карточку
+    const cardElement = addCard(item, deleteCard);
+
+    // добавить карточку в DOM
+    const cardContainer  = document.querySelector('.page__content .content .places .places__list');
+    cardContainer.append(cardElement);
+  });
+}
+
+showAllCards();
